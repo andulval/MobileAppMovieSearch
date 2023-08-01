11085c79985de14343841daed17f0145d2bb895d
andulval <mzadlo@interia.pl>
2023-08-01 13:56:22 +0200

    gitignore updated

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Image } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test app</Text>
+      <Text>Test app. Showing cat!</Text>
       <Image
         source={{
           uri: "https://reactnative.dev/docs/assets/p_cat2.png",
