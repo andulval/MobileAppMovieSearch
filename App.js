8a3f14419a84624abe0ec949d419f3593d92fac9
andulval <mzadlo@interia.pl>
2023-08-01 13:50:30 +0200

    init packages

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Test app</Text>
+      <Image
+        source={{
+          uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#5015",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
