456b81eb0f170f1e57ffe2585d405d1dae12e100
andulval <mzadlo@interia.pl>
2023-08-03 11:47:09 +0200

    add required data to card and detail pages

--- src/routes/details/details.component.tsx
+++ src/routes/details/details.component.tsx
@@ -1,16 +1,58 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../../../App";
 import { Image } from "react-native";
 
+import { fetchMovieDetail, FETCH_TYPES_TMDB } from "../../utils/fetchTMDB";
+
 type Props = NativeStackScreenProps<RootStackParams, "MovieDetails">;
 
+export type MovieDetailsProps = {
+  id: string;
+  title: string;
+  overview: string;
+  poster_path: string;
+  vote_count: number;
+  genres: { id: string; name: string }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+};
+
+const INIT_MOVIE_DETAILS = {
+  id: "",
+  title: "",
+  overview: "",
+  poster_path: "",
+  vote_count: 0,
+  genres: [{ id: "", name: "" }],
+  production_countries: [{ iso_3166_1: "1", name: "" }],
+};
+
 const MovieDetails = ({ route }: Props) => {
-  const { id, title, overview, poster_path, vote_count } = route.params;
+  const { id } = route.params;
+  const [movie, setMovie] = useState<MovieDetailsProps>(INIT_MOVIE_DETAILS);
+
+  const {
+    title,
+    overview,
+    poster_path,
+    vote_count,
+    genres,
+    production_countries,
+  } = movie;
+
   const urlImg = poster_path
     ? `https://image.tmdb.org/t/p/w154/${poster_path}`
     : "https://placehold.co/154x231/png/?text=No\\nImage\\nAvailable"; //if no image - show placeholder
+
+  useEffect(() => {
+    const fetchMovieD = async () => {
+      const fetch = await fetchMovieDetail(FETCH_TYPES_TMDB.details, id);
+      setMovie(fetch);
+      //   console.log("fetchMovieD", fetch.genres.map((genre)=>));
+    };
+    fetchMovieD();
+  }, []); // run when searchPhrase changes
 
   return (
     <View style={styles.item}>
@@ -23,12 +65,24 @@ const MovieDetails = ({ route }: Props) => {
         style={{ width: 154, height: 231 }}
       />
       {overview.length !== 0 ? (
-        <Text>`overview: {overview && overview}`</Text>
+        <Text>overview: {overview}</Text>
       ) : (
         <Text>No overiew available</Text>
       )}
-      <Text>`id: {id}`</Text>
-      <Text>`vote_count: {vote_count}`</Text>
+      <Text>vote_count: {vote_count}</Text>
+      <Text>genre:</Text>
+      {genres.map((genre) => {
+        return <Text key={genre.id}>{genre.name}</Text>;
+      })}
+      <Text>production_countries:</Text>
+      {production_countries.map((production_country) => {
+        return (
+          <Text key={production_country.iso_3166_1}>
+            {production_country.name}
+          </Text>
+        );
+      })}
+      <Text>vote_count: {vote_count}</Text>
     </View>
   );
 };
